08c385a3f763666d4c5d39104fb0c02d0aa9535f
Jamie Peloquin <jamie@jamiepeloquin.com>
2022-10-05 15:54:48 -0400

    Added packages and initial JS files.

---
+++ bin/index.js
@@ -0,0 +1,12 @@
+const appName = "AWS IAM Credentials Manager";
+const requiredBinaries = ['aws'];
+const debugAws = require('debug')('aws-sdk');
+
+try {
+    (async () => {
+
+    })()
+} catch (err) {
+    display.error(err.message)
+    process.exit(1)
+}
